e07b11a77caeb5f42379e8cc34793aff653aff1f
shyamshr93 <shyamshr93@gmail.com>
2020-07-27 22:50:38 +0530

    Added Semester Fragment

--- app/src/main/java/com/example/msinotes/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/msinotes/ui/home/HomeFragment.java
@@ -4,15 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.msinotes.R;
+import com.example.msinotes.ui.SemesterFragment;
 
 public class HomeFragment extends Fragment {
 
@@ -24,10 +28,17 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 //        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        Button btnFrag = (Button)root.findViewById(R.id.btnSem1);
+        final FrameLayout frmMain = (FrameLayout)root.findViewById(R.id.frame_home);
+        btnFrag.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
+            public void onClick(View v)
+            {
+                frmMain.setVisibility(View.VISIBLE);
+                FragmentTransaction frag = getFragmentManager().beginTransaction();
+                frag.replace(R.id.frame_home, new SemesterFragment());
+                frag.commit();
             }
         });
         return root;
